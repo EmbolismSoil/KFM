1e044c6f48de2cd493f272e5cb3f060e3ccfc7c0
EmbolismSoil <lika52090@gmail.com>
2020-04-24 21:48:09 +0800

    [UPDATE] 增加多线程训练

--- Learner.h
+++ Learner.h
@@ -30,7 +30,7 @@ class SGDLearner : public Learner
 {
 public:
 
-    SGDLearner(loss_grad_type const& loss_grad, output_grad_type const& output_grad, loss_op_type const& loss_op, infer_op_type const& infer_op, double lr=0.001):
+    SGDLearner(loss_grad_type const& loss_grad, output_grad_type const& output_grad, loss_op_type const& loss_op, infer_op_type const& infer_op, double lr=0.001, double lambda=0.001):
         _loss_grad(loss_grad),
         _output_grad(output_grad),
         _loss_op(loss_op),
@@ -144,9 +144,11 @@ public:
         _output_grad(yhat, g_output);
         
         auto g_loss_output = g_loss.array() * g_output.array();
-        db = -_lr*(g_loss_output.sum()/n);
+        db = -_lr*(g_loss_output.sum()/n) - _lr*_lambda*paramters.b;
         Eigen::Matrix<double, 1, Eigen::Dynamic> gt = g_loss_output.transpose();
+        Eigen::MatrixXd Wl2 = -_lr*_lambda*paramters.W;
         dW = -_lr*((gt * X).array() / n);
+        dW = dW + Wl2;
         
         Eigen::MatrixXd mask = g_loss_output.replicate(1, X.cols());
         Eigen::MatrixXd p1 = (X.array() * mask.array()).matrix().transpose();
@@ -159,7 +161,9 @@ public:
             p2 = t + p2;
         }
 
+        Eigen::MatrixXd Vl2 = -_lr*_lambda*paramters.V;
         dV = (-_lr/n) * (p1*XV -  p2.matrix());
+        dV = dV + Vl2;
 
         return _loss_op(y, yhat);
     }
@@ -170,6 +174,7 @@ private:
     loss_op_type _loss_op;
     infer_op_type _infer_op;
     double _lr;
+    double _lambda;
 };
 
 class LearnerFactory
@@ -184,14 +189,14 @@ public:
         return factory;
     }
 
-    std::shared_ptr<Learner> create(LEARNER_t learner, OUTPUT_t output, double lr=0.001)
+    std::shared_ptr<Learner> create(LEARNER_t learner, OUTPUT_t output, double lr=0.001, double lambda=0.001)
     {
         auto infer_op = std::bind(_fm_infer, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, output);
         if (learner == SGD){
             if (output == LINER){
-                return std::make_shared<SGDLearner>(mse_grad, liner_grad, mse_op, infer_op, lr);
+                return std::make_shared<SGDLearner>(mse_grad, liner_grad, mse_op, infer_op, lr, lambda);
             }else if (output == SIGMOID){
-                return std::make_shared<SGDLearner>(logloss_grad, sigmoid_grad, logloss_op, infer_op, lr);
+                return std::make_shared<SGDLearner>(logloss_grad, sigmoid_grad, logloss_op, infer_op, lr, lambda);
             }else{
                 return nullptr;
             }
