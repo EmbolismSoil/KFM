8a6136ca5e38369072e9f76c33a90394cb8c86ef
EmbolismSoil <lika52090@gmail.com>
2020-04-28 00:30:39 +0800

    [UPDATE] 需要编写makefile

---
+++ python/FMRegressor.cpp
@@ -0,0 +1,23 @@
+#include "FMRegressor.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/numpy.h>
+
+static
+KFM::FMRegressor create(Eigen::Index const ndim=64, double lr=0.0001, double gamma=0.01, double lambda=0.1, int njobs=-1, int max_step=-1)
+{
+    return KFM::FMRegressor(ndim, lr, gamma, lambda, njobs, max_step);
+}
+
+namespace py=pybind11;
+
+PYBIND11_MODULE(KFM, m)
+{
+    m.doc() = "FM Model";
+    py::class_<KFM::FMRegressor>(m, "FMRegressor")
+        .def(py::init<Eigen::Index, double, double, double, int, int>(), py::arg("ndim")=Eigen::Index(64), py::arg("lr")=double(0.0001), py::arg("gamma")=double(0.01), py::arg("lambda")=double(0.1), py::arg("njobs")=1, py::arg("max_step")=int(1))
+        .def("fit", &KFM::FMRegressor::fit, py::arg("X"), py::arg("y"), py::arg("batch_size")=int(100), py::arg("epoch")=int(5))
+        .def("predict", &KFM::FMRegressor::predict)
+        .def("save", &KFM::FMRegressor::save)
+        .def("load", &KFM::FMRegressor::load);
+}
